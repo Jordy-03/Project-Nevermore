864396578ae6e2d5052858cc934dcde781ed413d
Jordy Ruiz <jr03@uw.edu>
2025-08-14 23:19:47 -0700

    Implemented PoC for order page

--- my-react-app/client/src/pages/OrderPage.jsx
+++ my-react-app/client/src/pages/OrderPage.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function OrderPage() {
-    {/* Creating form data */} 
+    {/* Creating form data */ }
     const [formData, setFormData] = useState({
         firstName: '',
         lastName: '',
@@ -17,10 +17,28 @@ function OrderPage() {
     });
 
     return (
-    <div>
-        <h1>ORDER PAGE</h1>
-    </div>
-  );
+        <div>
+            <h1 className="site-header">
+                <nav>
+                    <Link to="/" className="logo">MyStore</Link>
+                    <ul className="nav-links">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/order" className="active">Orders</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                    </ul>
+                </nav>
+            </h1>
+
+            <main className="order-container">
+                {/* …rest of your JSX… */}
+            </main>
+
+            <footer className="site-footer">
+                <p>© 2025 MyStore. All rights reserved.</p>
+            </footer>
+        </div>
+
+    );
 };
 
 export default OrderPage;
